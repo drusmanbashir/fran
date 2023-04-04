7a45311912bc173ed20806b7ef2ed77a55821e70
Usman Bashir <drusmanbashir@gmail.com>
2023-04-04 12:24:58 +0100

    ..

--- fran/run/project_init.py
+++ fran/run/project_init.py
@@ -10,13 +10,16 @@ def main(args):
     input_folders = args.input_folders
     P = Project(project_title=project_title); 
     print("Project: {0}".format(project_title))
-    P.create_project(args.input_folders)
-    P.set_raw_data_sources(input_folders)
-    P.populate_raw_data_folder()
-    P.create_train_valid_folds()
-    pp(P.proj_summary)
-    P.save_summary()
-    P.raw_data_imgs
+    if not args.delete==True:
+        P.create_project(args.input_folders)
+        P.set_raw_data_sources(input_folders)
+        P.populate_raw_data_folder()
+        P.create_train_valid_folds()
+        pp(P.proj_summary)
+        P.save_summary()
+        P.raw_data_imgs
+    else:
+        P.delete()
 
 
 # %%
@@ -26,6 +29,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create new project or manage existing ones")
     parser.add_argument("-t", help="project title")
     parser.add_argument("-i","--input-folders" , help="Dataset parent folder containing subfolders 'images' and 'masks'",nargs='+')
+    parser.add_argument("-d" ,"--delete" ,action='store_true')
 
 # %%
     args = parser.parse_known_args()[0]
