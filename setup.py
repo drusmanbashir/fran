0dc954cf0fc6cc46329afb0d92aaec93ec2ecc4c
ub <drusmanbashir@gmail.com>
2025-03-12 11:42:30 +0000

    pip

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
 import os
 with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), encoding="utf-8") as f:
-    requirements = f.read().splitlines()
+    all_requirements = f.read().splitlines()
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-development_only = ["gudhi", "nurbspy",""]
+development_only = ["gudhi", "nurbspy"]
+
+# Filter out development-only requirements using partial string matching
+requirements = [
+    req for req in all_requirements 
+    if not any(dev_req in req for dev_req in development_only)
+]
+
 
 setuptools.setup(
     name="fran",  # Package name
