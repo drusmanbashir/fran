18093d3037b0992113e45e592ed02a68fcf4438a
ub <drusmanbashir@gmail.com>
2025-12-17 11:53:09 +0000

    bugs

--- fran/run/project_init.py
+++ fran/run/project_init.py
@@ -1,24 +1,28 @@
 # %%
 import argparse
-from utilz.string import headline
-from fran.managers.project import Project
-from fran.data.dataregistry import DS
-from fran.configs.parser import MNEMONICS
-
 # very top of project_init.py (above any 3rd-party imports)
 import os
-os.environ.setdefault("OPENBLAS_NUM_THREADS","1")
-os.environ.setdefault("OMP_NUM_THREADS","1")
-os.environ.setdefault("MKL_NUM_THREADS","1")
-os.environ.setdefault("OPENBLAS_NUM_THREADS","1")
-os.environ.setdefault("NUMEXPR_NUM_THREADS","1")
-os.environ.setdefault("MPLBACKEND","Agg")           # matplotlib headless
-os.environ.setdefault("QT_QPA_PLATFORM","offscreen")# belt & braces
-os.environ.setdefault("OPENCV_LOG_LEVEL","ERROR")
-os.environ.setdefault("ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS","1")
+
+from utilz.string import headline
+
+from fran.configs.parser import MNEMONICS
+from fran.data.dataregistry import DS
+from fran.managers.project import Project
+
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+os.environ.setdefault("MKL_NUM_THREADS", "1")
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+os.environ.setdefault("NUMEXPR_NUM_THREADS", "1")
+os.environ.setdefault("MPLBACKEND", "Agg")  # matplotlib headless
+os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")  # belt & braces
+os.environ.setdefault("OPENCV_LOG_LEVEL", "ERROR")
+os.environ.setdefault("ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS", "1")
 # optional noise reducers:
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL","3")
-os.environ.setdefault("CUDA_VISIBLE_DEVICES","") 
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")
+os.environ.setdefault("CUDA_VISIBLE_DEVICES", "")
+
+
 def main(args):
     headline("Arguments:")
     print(f"args.title: {args.title}")
@@ -27,7 +31,7 @@ def main(args):
     print(f"args.test: {args.test}")
     print(f"args.num_processes: {args.num_processes}")
     multiprocess = False if args.num_processes == 1 else True
-    
+
     P = Project(project_title=args.title)
 
     if not P.db.exists():
@@ -35,12 +39,15 @@ def main(args):
     if args.datasources:
         datas = [DS[name] for name in args.datasources]
         P.add_data(datasources=datas, test=args.test, multiprocess=multiprocess)
-    P.maybe_store_projectwide_properties(overwrite=False,multiprocess=multiprocess)
+    P.maybe_store_projectwide_properties(overwrite=False, multiprocess=multiprocess)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage FRAN projects")
     parser.add_argument("-t", "--title", help="Project title")
-    parser.add_argument("-m", "--mnemonic", help="Mnemonic, must be in MNEMONICS: {}".format(MNEMONICS))
+    parser.add_argument(
+        "-m", "--mnemonic", help="Mnemonic, must be in MNEMONICS: {}".format(MNEMONICS)
+    )
     parser.add_argument(
         "-n",
         "--num-processes",
@@ -48,9 +55,13 @@ if __name__ == "__main__":
         help="number of parallel processes. If 1 (default), a single process is used",
         default=1,
     )
-
     # parser.add_argument("-n", "--num")
-    parser.add_argument("--datasources", nargs="*", default=[], help="Datasources to add, i.e., {}".format(DS.__repr__()))
+    parser.add_argument(
+        "--datasources",
+        nargs="*",
+        default=[],
+        help="Datasources to add, i.e., {}".format(DS.__repr__()),
+    )
     parser.add_argument("--test", action="store_true", help="Mark datasources as test")
 # %%
     args = parser.parse_known_args()[0]
@@ -59,8 +70,6 @@ if __name__ == "__main__":
     # args.mnemonic = 'litsmall'
     # args.datasources = ['litsmall']
 
-
 # %%
     main(args)
 # %%
-
