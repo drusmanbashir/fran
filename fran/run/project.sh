f062bb1b2b910625f61b6b7a57dea86a1eba78eb
ub <drusmanbashir@gmail.com>
2026-02-28 21:50:56 +0000

    this

--- fran/run/project.sh
+++ fran/run/project.sh
@@ -10,5 +10,5 @@
 # python  analyze_resample.py -t totalseg -p 0
 # python project_init.py  -t lungs -i  /s/datasets_bkp/Task06Lung/
 
-python  analyze_resample.py -t colon -p 0
-python   project_init.py -t colon  --mnemonic colon  --datasources colonmsd10
+python   project_init.py -t pancreas  --mnemonic pancreas  --datasources pancreasmsd07
+python  analyze_resample.py -t pancreas -p 0
