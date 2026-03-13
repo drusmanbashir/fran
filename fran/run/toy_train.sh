c989d3186622110fb5ccd76db1ca3715b21db630
ub <drusmanbashir@gmail.com>
2026-03-13 00:19:45 +0000

    a

--- fran/run/toy_train.sh
+++ fran/run/toy_train.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-# python train.py -t lits32 -d [0] --bs 8 -f 2
-# python  train.py -t litsmc -d [0]  --bs 10 -f 1 -bsf  -e 500
-# python  train.py -t litsmc -r LITS-811 -e 500 --lr 11e-4 -b 8
-# python  train.py -t litsmc -r LITS-940 -e 500  -d [1]  
-python train.py \
+python train_retry.py \
   --project kits \
   --plan-num 3 \
   --fold 1 \
   --epochs 500 \
+  --bs 4 \
   --bsf false \
-  --bs 18 \
-  --devices [1] \
+  --devices "[1]" \
   --wandb true \
-  --train-indices 40 \
-  --val-every-n-epochs 5
+  --train-indices 20 \
+  --val-every-n-epochs 1
+
+# python train.py -t lits32 -d [0] --bs 8 -f 2
+# python  train.py -t litsmc -d [0]  --bs 10 -f 1 -bsf  -e 500
+# python  train.py -t litsmc -r LITS-811 -e 500 --lr 11e-4 -b 8
+# python  train.py -t litsmc -r LITS-940 -e 500  -d [1]  
+# python train.py \
+#   --project kits \
+#   --plan-num 3 \
+#   --fold 1 \
+#   --epochs 500 \
+#   --bsf false \
+#   --bs 18 \
+#   --devices [1] \
+#   --wandb true \
+#   --train-indices 40 \
+#   --val-every-n-epochs 5
 # python  train.py -t lungs  -e 500 --lr 11e-3 …-b 2
 
