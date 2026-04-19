5e84f264d352908e0307c8a0ce94256a4b16cedd
ub <drusmanbashir@gmail.com>
2026-04-20 00:43:24 +0100

    fixes

--- fran/run/datasource_init.py
+++ fran/run/datasource_init.py
@@ -6,29 +6,40 @@ from pathlib import Path
 from fran.data.datasource import Datasource
 
 
-def parse_args() -> argparse.Namespace:
+def main(args: argparse.Namespace) -> None:
+    folder = Path(args.folder).expanduser().resolve()
+    ds = Datasource(folder=folder, name=args.mnemonic)
+    ds.process(
+        return_voxels=False,
+        num_processes=args.num_processes,
+        multiprocess=args.num_processes > 1,
+    )
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Initialise a Datasource and process new cases."
     )
     parser.add_argument("folder", help="Datasource folder containing images/ and lms/")
     parser.add_argument("mnemonic", help="Datasource name/mnemonic")
     parser.add_argument(
-        "n_processes",
+        "-n",
+        "--num-processes",
         type=int,
+        default=None,
+        dest="num_processes",
         help="Number of worker processes for Datasource.process()",
     )
-    return parser.parse_args()
-
-
-def main(args: argparse.Namespace) -> None:
-    folder = Path(args.folder).expanduser().resolve()
-    ds = Datasource(folder=folder, name=args.mnemonic)
-    ds.process(
-        return_voxels=False,
-        num_processes=args.n_processes,
-        multiprocess=args.n_processes > 1,
+    parser.add_argument(
+        "n_processes",
+        nargs="?",
+        type=int,
+        default=None,
+        help=argparse.SUPPRESS,
     )
-
-
-if __name__ == "__main__":
-    main(parse_args())
+    args = parser.parse_known_args()[0]
+    if args.num_processes is None:
+        args.num_processes = 1 if args.n_processes is None else args.n_processes
+    if args.num_processes < 1:
+        parser.error("--num-processes must be >= 1")
+    main(args)
